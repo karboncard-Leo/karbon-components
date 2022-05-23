3e067163ebbdec5aeaa4e2fe15e123d444d3e5b9
leo <qiuyanxi1992@gmail.com>
2022-05-23 14:07:15 +0800

    style(view): update styles in pageContainer component

--- src/components/page-container/style.ts
+++ src/components/page-container/style.ts
@@ -1,44 +1,54 @@
 import styled from 'styled-components';
-
 export const Container = styled.section`
-  padding: 24px;
-  height: 100%;
-  > header {
-    position: relative;
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 24px;
-  }
-  .karbon_page_container_title {
-    color: rgba(0, 0, 0, 0.85);
-    font-weight: 500;
-    font-size: 24px;
-    font-family: 'Roboto';
-    font-style: normal;
+  &.karbon_page_container {
+    height: 100%;
+    padding: 24px;
+
+    > header {
+      position: relative;
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 24px;
+    }
+    .karbon_page_container_title {
+      font-weight: 400;
+      font-size: 24px;
+      font-family: 'Roboto Medium';
+    }
+
+    .karbon_page_container_extra {
+      align-self: flex-end;
+    }
+    .ant-tabs-tab {
+      padding-top: 0;
+    }
+    .ant-tabs-tab-active .ant-tabs-tab-btn {
+      color: rgba(0, 0, 0, 0.85);
+    }
+    .ant-tabs-tab .ant-tabs-tab-btn {
+      color: rgba(0, 0, 0, 0.45);
+    }
+    .ant-tabs-ink-bar {
+      border: 1px solid #000000;
+    }
+    .karbon_page_container_content {
+      padding: 24px;
+      background-color: white;
+    }
   }
 
-  .karbon_page_container_extra {
-    align-self: flex-end;
-  }
-  .ant-tabs-tab {
-    padding-top: 0;
-  }
-  .ant-tabs-tab-active .ant-tabs-tab-btn {
-    color: rgba(0, 0, 0, 0.85);
-  }
-  .ant-tabs-tab .ant-tabs-tab-btn {
-    color: rgba(0, 0, 0, 0.45);
-  }
-  .ant-tabs-ink-bar {
-    border: 1px solid #000000;
-  }
-  .karbon_page_container_content {
-    padding: 24px;
-    background-color: white;
-  }
   @media (max-width: 768px) {
-    .karbon_page_container_title {
-      font-size: 1.25rem;
+    &.karbon_page_container {
+      padding-right: 0;
+      padding-left: 0;
+      > header,
+      > div:nth-of-type(1) {
+        padding-right: 24px;
+        padding-left: 24px;
+      }
+    }
+    .karbon_page_container_content {
+      padding: 16px 24px;
     }
   }
 `;
